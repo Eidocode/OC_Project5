10b21dca535a1f36d0e6a70631d0436270096db7
David Bouzerar <eidocode@orange.fr>
2020-05-04 18:36:04 +0200

    ADD Class Category

---
+++ category.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+from random import randrange
+
+URL = 'https://fr.openfoodfacts.org/'
+
+class Category:
+    
+    def __init__(self, nb_cat):
+        self.nb_cat = nb_cat
+        self.list_categories = self.get_random_categories
+    
+
+    @property
+    def _get_categories(self):
+        list_cat = []
+        list_cat_filtered = []
+
+        url_cat = requests.get(URL + 'categories.json')
+        json_cat = url_cat.json()
+        list_cat = json_cat.get('tags')
+
+        for category in list_cat:
+            if (category["products"] >= 50 and category["id"].startswith('fr')) :
+                list_cat_filtered.append(category)
+        return list_cat_filtered
+
+    @property
+    def get_random_categories(self):
+        self.list_categories = []
+        lst = self._get_categories
+
+        for i in range(self.nb_cat):
+            index = randrange(0, len(lst))
+            self.list_categories.append(lst[index])
+        return self.list_categories
+
+
+# test = Category(10)
+# for category in test.list_categories:
+#     print("Name : ", category["name"], " | Products : ", category["products"], " | ID : ", category["id"], " | URL : ", category["url"])
