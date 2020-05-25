9131aef4776f9efc4632b4603dea29dfe3296932
Eidocode <eidocode@orange.fr>
2020-05-26 00:17:53 +0200

    Add category and product classes. Used to interact with categories and products

--- category.py
+++ category.py
@@ -1,11 +1,127 @@
+from colorama import init, deinit, Fore, Back, Style
+
 import constants as const
 
+from database_manager import DatabaseManager
+from api_handler_categories import ApiHandlerCategories
+
+
+init(autoreset=True)
 
 class Category:
+    """
+    Class used to manipulate categories
+
+    ...
+
+    Attributes
+    ----------
+    categories_in_db : list
+        List which contains all categories present in database
+    
+    list_json_id : list
+        List which contains values of json_id field in database
+    
+    Methods
+    -------
+    _get_from_api(nb_cat)
+        Analyze categories returns by ApiHandlerCategories Class.
+    
+    set_to_db(datas_to_insert)
+        Set categories to database
+    
+    get_all_from_db
+        Returns a list of all categories present in database
+    
+    exist_in_db
+        Check if json_id exists in database and returns True or False
+    """
     
     def __init__(self):
-        pass
+        self.categories_in_db = self.get_all_from_db()
+        
+        self.list_json_id = []
+        for cat in self.categories_in_db:
+            self.list_json_id.append(cat[2])
 
-    def get_all(self):
-        pass
+    def _get_from_api(self, nb_cat):
+        """Analyze categories returns by ApiHandlerCategories Class. For each 
+        category, check if it already exists in list or database. Returns a 
+        list without any duplicate category.
+
+        Parameters
+        ----------
+        nb_cat : int
+            Number of categories for instantiation of ApiHandlerCategories
+        """
+
+        list_categories = []
+        api_datas = ApiHandlerCategories(nb_cat)
+
+        for category in api_datas.categories:
+            if not self.exist_in_db(category['id']):
+                datas = (category['name'], category['id'], category['url'])
+                list_categories.append(datas)
+                print(category['name'] + " added to database...")
+            else :
+                print(Fore.YELLOW + category['name'] + " already in list or database...")
+        
+        return list_categories
     
+    def set_to_db(self, datas_to_insert):
+        """Set categories to database with DatabaseManager Class
+
+        Parameters
+        ----------
+        datas_to_insert : tuple
+            tuple of data to insert in database
+        """
+
+        db_manager = DatabaseManager()
+
+        insert_query = """INSERT INTO Categories (name, json_id, url)
+                            VALUES
+                            (%s, %s, %s)"""
+        
+        db_manager.set_query(insert_query, datas_to_insert)
+        db_manager._destroy()
+    
+    def get_all_from_db(self):
+        """Returns a list of all categories present in database"""
+        
+        db_manager = DatabaseManager()
+        list_categories = []
+
+        query_get_categories = """SELECT * FROM Categories"""
+        
+        list_categories = db_manager.get_query(query_get_categories)
+        return list_categories
+
+    def exist_in_db(self, json_id):
+        """Check if json_id exists in database and returns True or False
+        If json_id not present in list_json_id, add it to prevent duplicate 
+        category.
+
+        Parameters
+        ----------
+        json_id = string
+            Contains json_id category value
+        """
+        if json_id in self.list_json_id:
+            return True
+        else:
+            self.list_json_id.append(json_id)
+
+        return False
+
+    def _destroy(self):
+        print(Fore.LIGHTYELLOW_EX + "Instance Category has been deleted")
+        del self
+
+
+# test = Category()
+# liste = test._get_from_api(const.NB_CATEGORIES_TO_GET)
+# test.set_to_db(liste)
+# test._destroy()
+
+deinit()
--- product.py
+++ product.py
@@ -1,58 +1,124 @@
-import requests
-import json
-
-from random import randrange
-from math import ceil
-
-from category import Category
+from colorama import init, deinit, Fore, Back, Style
 
 import constants as const
 
+from database_manager import DatabaseManager
+from api_handler_products import ApiHandlerProducts
+from category import Category
+
+
+init(autoreset=True)
 
 class Product:
+    
+    def __init__(self):
+        pass
 
-    def __init__(self, category_url):
-        self.category_url = category_url
-        self.nb_products = 0
-        self.nb_pages = 0
-        self.products = self._get_products
+    def get_barcodes_from_db(self):
+        db_manager = DatabaseManager()
+        barcodes = []
+        barcodes_from_db = []
+
+        query_get_barcode = """SELECT barcode FROM Products """
+
+        barcodes_from_db = db_manager.get_query(query_get_barcode)
+        for code in barcodes_from_db:
+            barcodes.append(code[0])
         
-    @property
-    def _get_products(self):
-        url_products = requests.get(self.category_url + ".json")  # Request Category URL
-        json_products = url_products.json()
+        db_manager._destroy()
+        return barcodes
+
+    def test_condition(self, condition, message):
+        pass_test = True
+
+        if condition:
+            pass_test = False
+            print(Fore.YELLOW + message)
+
+        return pass_test
+    
+    def test_product(self, product, list_barcodes):
+        test_pkey = self.test_condition(not('product_name' in product.keys()),
+                            "Unable to find key product_name...")
+        
+        if test_pkey:
+            test_pcode = self.test_condition(product['code'] in list_barcodes,
+                                product['product_name'] + " already in list or database...")
             
-        self.nb_products = int(json_products.get('count'))  # Get Category number of products
-        self.nb_pages = ceil(self.nb_products / const.PRODUCTS_PER_PAGE)  # Get Category number of pages
+            test_pname = self.test_condition(product['product_name'] == "",
+                                "Product doesn't have a name...")
+        else :
+            return False
+        
+        return test_pkey == test_pcode == test_pname
+    
+    def _set_key_value(self, product, value):
+        if value in product.keys():
+            return product[value]
+        return None
 
-        list_all_prod = []
-        self.products = []
+    def _get_value_for_db(self, categorie, product):
+        product_name = product['product_name']
+        product_brand = self._set_key_value(product, 'brands')
+        product_description = self._set_key_value(product, 'ingredients_text')
+        product_nutriscore = self._set_key_value(product, 'nutriscore_grade')
+        product_places = self._set_key_value(product, 'purchase_places')
+        product_stores = self._set_key_value(product, 'stores')
+        product_barcode = product['code']
 
-        i = 1
-        for i in range(self.nb_pages):
-            this_url = self.category_url + "/" + str(i) + ".json"
-            this_url_products = requests.get(this_url)
-            json_this_url_products = this_url_products.json()
-            list_this_url_products = json_this_url_products.get('products')
+        product_values =  (product_name, product_brand, product_description, product_nutriscore,
+                            categorie[0], product_places, product_stores, product_barcode)
+        
+        return product_values
+
+
+    def _get_from_api(self, nb_prod):
+        list_products = []
+        barcodes = []
+        category = Category()
+        categories_in_db = category.categories_in_db
+
+        barcodes = self.get_barcodes_from_db()
+        i = 0
+        for category in categories_in_db:
+            print(Fore.BLUE + str(i+1) + " : " + category[1])
+            products = ApiHandlerProducts(category[3], nb_prod)
+            nb = 0
+            for product in products.products:
+                is_viable = self.test_product(product, barcodes)
+                if is_viable:
+                    this_product = self._get_value_for_db(category, product)
+                    list_products.append(this_product)
+                    barcodes.append(product['code'])
+                    print(Fore.GREEN + str(nb+1) + ". " + product['product_name'] + " viable...")
+                    nb += 1
+                else:
+                    print(Fore.RED + "Product not viable...")
+                    continue
             
-            for product in list_this_url_products:
-                list_all_prod.append(product)
+            i += 1
+
+        return list_products
+    
+    def set_to_db(self, datas_to_inject):
+        db_manager = DatabaseManager()
+
+        insert_query = """INSERT INTO Products
+                            (name, brand, description, nutriscore, category_id, places, stores, barcode)
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+        
+        db_manager.set_query(insert_query, datas_to_inject)
+        db_manager._destroy()
+    
+    def _destroy(self):
+        print(Fore.LIGHTYELLOW_EX + "Instance Product has been deleted")
+        del self
 
 
-        # rand_num_page = randrange(1, self.nb_pages)
-        # rand_url = self.category_url + "/" + str(rand_num_page) + ".json"
-        # rand_url_products = requests.get(rand_url)  # Get content of a random page number
-        # json_rand_url_products = rand_url_products.json()
-        # list_rand_url_products = json_rand_url_products.get('products')  # Get products in random URL
+# product = Product()
+# liste = product._get_from_api(const.PRODUCTS_PER_CATG)
 
-        # index = randrange(0, len(list_rand_url_products))
-        # self.products = list_rand_url_products[index]
-        self.products = list_all_prod
-        return self.products
+# product.set_to_db(liste)
+# product._destroy()
 
-prod = Product('https://fr.openfoodfacts.org/categorie/wraps')
-print(len(prod.products))
-
-for i in range(10):
-    rand_index = randrange(1, len(prod.products))
-    print(prod.products[rand_index]['product_name'])
+deinit()
