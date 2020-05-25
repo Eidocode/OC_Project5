717e77b2b1e23f0b14e4bb59a7324ad5e8be3c23
Eidocode <eidocode@orange.fr>
2020-05-26 00:16:55 +0200

    Add database_manager and documentation..

--- database_manager.py
+++ database_manager.py
@@ -4,13 +4,32 @@ from colorama import init, deinit, Fore, Back, Style
 
 import constants as const
 
-from api_handler_categories import ApiHandlerCategories
-
 
 init(autoreset=True)
 
 class DatabaseManager:
+    """ 
+    Class used to communicate (/w credentials) with SQL server.
+    Send "get"/"set" queries to database and return results...
     
+    ...
+
+    Attributes
+    ----------
+    sql_connx : tuple
+        Contains SQL server informations like credentials, @IP, database name...
+    
+    Methods
+    -------
+    get_query(query)
+        Returns results of a "get" query
+        example : 'SELECT id, name, json_id FROM Categories WHERE id > 5'
+    
+    set_query(query, datas)
+        Set datas to SQL database with a "set" query
+        example : 'INSERT INTO Categories (name, json_id, url) VALUES (%s, %s, %s)'
+    """
+
     def __init__(self):
         self.sql_connx = mysql.connector.connect(
                 host = const.SRV_IP,
@@ -20,13 +39,19 @@ class DatabaseManager:
             )
 
     def get_query(self, query):
+        """Returns results of a "get" query
+        
+        Parameters
+        ----------
+        query : str
+            query sent to database
+        """
+
         result = None
         try:
             self.sql_connx
-        
             cursor = self.sql_connx.cursor()
             print(Fore.GREEN + "Connection Established...")
-
             cursor.execute(query)
             result = cursor.fetchall()
             cursor.close()
@@ -38,16 +63,24 @@ class DatabaseManager:
             if (self.sql_connx.is_connected()):
                 self.sql_connx.close()
                 print(Fore.GREEN + "Mysql connection closed...")
-        
+
         return result
 
     def set_query(self, query, datas):
+        """Returns results of a "get" query
+        
+        Parameters
+        ----------
+        query : str
+            query sent to database
+        
+        datas : list or tuple
+            datas to be injected in database
+        """
         try:
             self.sql_connx
-        
             cursor = self.sql_connx.cursor()
             print(Fore.GREEN + "Connection Established...")
-
             cursor.executemany(query, datas)
             print(Fore.GREEN + str(cursor.rowcount) + " QUERY SUCCESSFULL...")
             self.sql_connx.commit()
@@ -66,6 +99,5 @@ class DatabaseManager:
     
     def _destroy(self):
         del self
-
 
 deinit()
