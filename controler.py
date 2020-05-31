ccf48d8a0ffa061a27ef88a8e8430d5b29450bdb
David Bouzerar <eidocode@orange.fr>
2020-05-31 22:08:30 +0200

    Added controler.py and Utils package

---
+++ controler.py
@@ -0,0 +1,118 @@
+from colorama import init, deinit, Fore, Back, Style
+
+from model.category import Category
+from model.product import Product
+
+
+class Controler:
+    
+    def __init__(self):
+        pass
+
+    def get_database_status(self):
+        self.category = Category()
+        self.product = Product()
+
+        nb_categories = len(self.category.categories_in_db)
+        nb_products = len(self.product.products_in_db)
+
+        self.category._destroy()
+        self.product._destroy()
+
+        status = "La base contient actuellement : {} catégories | {} produits.".format(nb_categories, nb_products)
+        return status
+    
+    def set_categories(self, nb_categories):
+        category = Category()
+
+        categories_to_set = category._get_from_api(nb_categories)
+        category.set_to_db(categories_to_set)
+        category._destroy()
+
+        print("Controler : set_categories done...")
+    
+    def set_products(self, nb_products):
+        product = Product()
+
+        products_to_set = product._get_from_api(nb_products)
+        product.set_to_db(products_to_set)
+        product._destroy()
+
+        print("Controler : set_products done...")
+
+    def set_products_in_category(self, nb_products, id_category):
+        product = Product()
+        category = Category()
+
+        this_category = category.get_one_from_db(id_category)
+        products_to_set = product._get_from_api(nb_products, this_category)
+        product.set_to_db(products_to_set)
+        
+        product._destroy()
+        category._destroy()
+
+        print("Controler : set_products done...")
+    
+    def get_all_categories_info(self):
+        category = Category()
+        
+        categories = category.get_all_from_db()
+        category._destroy()
+
+        for cat in categories:
+            cat_id = cat[0]
+            cat_name = cat[1]
+            print(str(cat_id) + " : " + cat_name)
+    
+    def get_category_info(self, id_category):
+        category = Category()
+        this_category = {}
+        category_info = category.get_one_from_db(id_category)
+
+        for c in category_info:
+            this_category['id'] = c[0]
+            this_category['name'] = c[1]
+            this_category['json_id'] = c[2]
+            this_category['url'] = c[3]
+
+        category._destroy()
+
+        if len(category_info) == 0:
+            return None
+        return this_category
+
+    def get_all_products_info(self, category_id):
+        product = Product()
+
+        products = product.get_all_from_a_category(category_id)
+        product._destroy()
+
+        for prod in products:
+            prod_id = prod[0]
+            prod_name = prod[1]
+            print(str(prod_id) + " : " + prod_name)
+        
+        print("--")
+        print(str(len(products)) + " produits présents dans cette catégories")
+    
+    def get_product_info(self, product_id, category_id):
+        product = Product()
+        this_product = {}
+        product_info = product.get_one_from_db(product_id)
+
+        for p in product_info:
+            this_product['id'] = p[0]
+            this_product['name'] = p[1]
+            this_product['brand'] = p[2]
+            this_product['description'] = p[3]
+            this_product['nutriscore'] = p[4]
+            this_product['category_id'] = p[5]
+            this_product['places'] = p[6]
+            this_product['stores'] = p[7]
+            this_product['barcode'] = p[8]
+        
+        product._destroy()
+
+        if int(this_product['category_id']) != int(category_id):
+            return None
+        return this_product
---
+++ utils/constants.py
@@ -0,0 +1,15 @@
+URL = 'https://fr.openfoodfacts.org/'  # /categorie/[name_cat].json
+
+QUERY_GET_CATEGORIES = """SELECT * FROM Categories"""
+
+NB_CATEGORIES_TO_GET = 10
+MIN_PRODUCTS_TO_FILTER = 100 
+PRODUCTS_PER_PAGE = 20
+PRODUCTS_PER_CATG = 20
+
+NB_TRY = 3
+
+SRV_IP = "192.168.1.77"
+USER_ID = "student"
+USER_PWD = "student"
+DB_NAME = "pur_beurre"
